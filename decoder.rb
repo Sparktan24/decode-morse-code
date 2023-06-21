7d124eb50ef3f65ebdba9a0ef7a0d0fb17a02976
Cisco <ciscophrenzy@gmail.com>
2023-06-21 13:43:19 -0600

    Add word method

--- decoder.rb
+++ decoder.rb
@@ -10,3 +10,15 @@ def decode_char(morse_code)
 
   morse_code_table[morse_code] || ''
 end
+
+def decode_word(morse_word)
+  morse_chars = morse_word.split
+  decoded_word = ''
+
+  morse_chars.each do |morse_char|
+    decoded_char = decode_char(morse_char)
+    decoded_word += decoded_char
+  end
+
+  decoded_word
+end
